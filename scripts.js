664dd13a145160b50f31950a27ba9f37fb8d3f18
Elrey Belmonti <elbelmonti@gmail.com>
2018-06-27 10:50:43 -0700

    follow video and added full screen

---
+++ scripts.js
@@ -0,0 +1,116 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+const fullScreenButton = player.querySelector('.full__screen');
+
+
+function togglePlay() {
+  video[video.paused ? 'play' : 'pause']();
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip() {
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  console.log(this.name);
+  console.log(this.value);
+}
+
+
+function handleProgress() {
+  const precent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${precent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+function GoInFullscreen(element) {
+  if (element.requestFullscreen)
+    element.requestFullscreen();
+  else if (element.mozRequestFullScreen)
+    element.mozRequestFullScreen();
+  else if (element.webkitRequestFullscreen)
+    element.webkitRequestFullscreen();
+  else if (element.msRequestFullscreen)
+    element.msRequestFullscreen();
+}
+
+function GoOutFullscreen() {
+  if (document.exitFullscreen)
+    document.exitFullscreen();
+  else if (document.mozCancelFullScreen)
+    document.mozCancelFullScreen();
+  else if (document.webkitExitFullscreen)
+    document.webkitExitFullscreen();
+  else if (document.msExitFullscreen)
+    document.msExitFullscreen();
+}
+
+function CurrentFullScreenElement() {
+  return (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement || null);
+}
+
+function IsFullScreenCurrently() {
+  return CurrentFullScreenElement() !== null
+}
+
+
+if (document.addEventListener) {
+  document.addEventListener('webkitfullscreenchange', exitHandler, false);
+  document.addEventListener('mozfullscreenchange', exitHandler, false);
+  document.addEventListener('fullscreenchange', exitHandler, false);
+  document.addEventListener('MSFullscreenChange', exitHandler, false);
+  console.log("full screen change");
+}
+
+function exitHandler() {
+  if (!document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
+    console.log("exit full screen");
+  }
+}
+
+function handleFullScreen() {
+
+  GoInFullscreen(video);
+
+}
+
+
+
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+// ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mousedup', () => mousedown = false);
+
+
+fullScreenButton.addEventListener('click', handleFullScreen);
